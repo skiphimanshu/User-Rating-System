31200f3903ecf52f39b8900b14dc7d26cd7bb6f2
Himanshu <skiphimanshu@gmail.com>
2025-02-01 23:37:28 +0530

    added security config

---
+++ UserService/UserService/src/main/java/com/user/config/interceptor/FiegnClientInterceptor.java
@@ -0,0 +1,30 @@
+package com.user.config.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.stereotype.Component;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+@Configuration
+@Component
+public class FiegnClientInterceptor implements RequestInterceptor{
+	
+	private OAuth2AuthorizedClientManager manager;
+
+	@Override
+	public void apply(RequestTemplate template) {
+		String token = manager.authorize(OAuth2AuthorizeRequest
+				.withClientRegistrationId("my-internal-client").principal("internal").build())
+				.getAccessToken().getTokenValue();
+        if (token != null) {
+        	
+            template.header("Authorization", "Bearer " + token);
+        }
+		
+	}
+}
